8d9081b1273ce99c44a0fb1e22f8cb4362be5130
Evgeni Pandurski <epandurski@gmail.com>
2018-11-10 20:34:32 +0200

    Allow passing signal names to the flush CLI command

--- flask_signalbus/cli.py
+++ flask_signalbus/cli.py
@@ -10,10 +10,33 @@ def signalbus():
 
 @signalbus.command()
 @with_appcontext
-def flush():
-    """Flush all pending signals."""
+@click.option('-e', '--exclude', multiple=True, help="Do not flush signals with the specified name.")
+@click.argument('signal_names', nargs=-1)
+def flush(signal_names, exclude):
+    """Send pending signals over the message bus.
 
-    signal_count = current_app.extensions['signalbus'].flush()
+    If a list of SIGNAL_NAMES is specified, flushes only those
+    signals. If no SIGNAL_NAMES are specified, flushes all signals.
+
+    """
+
+    signalbus = current_app.extensions['signalbus']
+    signal_names = set(signal_names)
+    exclude = set(exclude)
+    models_to_flush = signalbus.get_signal_models()
+    if signal_names and exclude:
+        click.echo('Warning: Specified both SIGNAL_NAMES and exclude option.')
+    if signal_names:
+        wrong_signal_names = signal_names - {m.__name__ for m in models_to_flush}
+        models_to_flush = [m for m in models_to_flush if type(m).__name__ in signal_names]
+    else:
+        wrong_signal_names = exclude - {m.__name__ for m in models_to_flush}
+    for name in wrong_signal_names:
+        click.echo('Warning: A signal with name "{}" does not exist.'.format(name))
+    models_to_flush = [m for m in models_to_flush if type(m).__name__ not in exclude]
+    signal_count = 0
+    for model in models_to_flush:
+        signal_count += signalbus.flush()
     if signal_count == 1:
         click.echo('{} signal has been successfully processed.'.format(signal_count))
     elif signal_count > 1:
