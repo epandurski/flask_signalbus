c6d6c270a867dc82253435b09ab61fdddd2ed1e0
Evgeni Pandurski <epandurski@gmail.com>
2018-11-21 21:28:14 +0200

    Add __doc__

--- setup.py
+++ setup.py
@@ -2,8 +2,23 @@
 Flask-SignalBus
 ---------------
 
-TODO
+Adds to Flask-SQLAlchemy the capability to *atomically* send messages
+(signals) over a message bus.
+
+The processing of each message involves three steps:
+
+  1. The message is recorded in the SQL database as a row in a table.
+
+  2. The message is sent over the message bus (RabbitMQ for example).
+
+  3. Message's corresponding table row is deleted.
+
+Normally, the sending of the recorded messages (steps 2 and 3) is done
+automatically after each transaction commit, but when needed, it can
+also be triggered explicitly with a method call, or through the Flask
+CLI.
 """
+
 import sys
 from setuptools import setup
 
