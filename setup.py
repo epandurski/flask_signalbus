c6313a79f7d92c4865911990d8ce2b3646c0e070
Evgeni Pandurski <epandurski@gmail.com>
2018-11-22 16:13:41 +0200

    Bump the version number to 0.3

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ pytest_runner = ['pytest-runner'] if needs_pytest else []
 
 setup(
     name='Flask-SignalBus',
-    version='0.2',
+    version='0.3',
     url='https://github.com/epandurski/flask_signalbus',
     license='MIT',
     author='Evgeni Pandurski',
