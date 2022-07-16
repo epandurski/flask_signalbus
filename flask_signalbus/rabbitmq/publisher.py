b7c6ae4e0957cb4fd0305472f5d6160f7cb65bda
Evgeni Pandurski <epandurski@gmail.com>
2022-07-16 16:29:32 +0300

    Work in progress on rabbitmq/publisher.py

--- flask_signalbus/rabbitmq/publisher.py
+++ flask_signalbus/rabbitmq/publisher.py
@@ -2,7 +2,6 @@
 # pylint: disable=C0111,C0103,R0205
 
 import logging
-import json
 import pika
 from threading import local
 
@@ -26,7 +25,15 @@ class RabbitmqPublisher(object):
     """
 
     def __init__(self, app):
-        self._state = local()
+        self._url = ''
+        self._state = state = local()
+        state.connection = None
+        state.channel = None
+        state.messages = []
+        state.exchange = ''
+        state.routing_key = ''
+        state.message_number = 0
+        state.deliveries = set()
         if app is not None:
             self.init_app(app)
 
@@ -35,11 +42,26 @@ class RabbitmqPublisher(object):
         self._url = app.config['FLASK_SIGNALBUS_AMQP_URL']
 
     @property
-    def _connection(self):
-        """The :class:`pika.SelectConnection` for the current thread. This
-        property may change without notice.
+    def _channel(self):
+        """The channel for the current thread. This property may change
+        without notice.
         """
-        connection = getattr(self._state, "connection", None)
+        return self._state.channel
+
+    @_channel.setter
+    def _channel(self, value):
+        state = self._state
+        old_channel = state.channel
+        if value is not old_channel:
+            if not (old_channel is None or old_channel.is_closed or old_channel.is_closing):
+                old_channel.close()
+            state.channel = value
+            state.deliveries = set()
+            state.message_number = 0
+
+    def _get_connection(self):
+        """Return the RabbitMQ connection for the current thread."""
+        connection = self._state.connection
         if connection is None:
             LOGGER.info('Connecting to %s', self._url)
 
@@ -54,32 +76,13 @@ class RabbitmqPublisher(object):
             )
         return connection
 
-    @_connection.deleter
-    def _connection(self):
+    def _kill_connection(self):
         self._channel = None
-        try:
-            connection = self._state.connection
-        except AttributeError:
-            pass
-        else:
+        connection = self._state.connection
+        if connection is not None:
             if not (connection.is_closed or connection.is_closing):
                 connection.close()
-            del self._state.connection
-
-    @property
-    def _channel(self):
-        """The channel for the current thread. This property may change
-        without notice.
-        """
-        return getattr(self._state, "channel", None)
-
-    @_channel.setter
-    def _channel(self, value):
-        c = self._channel
-        if not (c is None or c.is_closed or c.is_closing):
-            LOGGER.info('Closing the channel')
-            c.close()
-        self._state.channel = value
+            self._state.connection = None
 
     def _on_connection_open(self, connection):
         """This method is called by pika once the connection to RabbitMQ has
@@ -101,26 +104,24 @@ class RabbitmqPublisher(object):
         """
         LOGGER.error('Connection open failed: %s', err)
         troubled_connection.ioloop.stop()
-        if self._connection is troubled_connection:
-            del self._connection
+        if self._get_connection() is troubled_connection:
+            self._kill_connection()
 
     def _on_connection_closed(self, closed_connection, reason):
         """This method is invoked by pika when the connection to RabbitMQ is
         closed unexpectedly. Since it is unexpected, we will reconnect to
         RabbitMQ if it disconnects.
         """
-        LOGGER.warning('Connection closed: %s', reason)
+        LOGGER.info('Connection closed: %s', reason)
         closed_connection.ioloop.stop()
-        if self._connection is closed_connection:
-            del self._connection
+        if self._get_connection() is closed_connection:
+            self._kill_connection()
 
     def _on_channel_open(self, channel):
         """This method is invoked by pika when the channel has been opened.
         The channel object is passed in so we can make use of it.
         """
         LOGGER.info('Channel opened')
-        self._state.deliveries = set()
-        self._state.message_number = 0
         self._channel = channel
         channel.add_on_close_callback(self._on_channel_closed)
 
@@ -134,6 +135,9 @@ class RabbitmqPublisher(object):
         LOGGER.info('Issuing Confirm.Select RPC command')
         channel.confirm_delivery(self._on_delivery_confirmation)
 
+        assert channel.is_open
+        self._send_messages(channel)
+
     def _on_channel_closed(self, closed_channel, reason):
         """Invoked by pika when RabbitMQ unexpectedly closes the channel.
         Channels are usually closed if you attempt to do something that
@@ -143,7 +147,7 @@ class RabbitmqPublisher(object):
         """
         LOGGER.warning('Channel %i was closed: %s', closed_channel, reason)
         if self._channel is closed_channel:
-            del self._connection
+            self._kill_connection()
 
     def _on_delivery_confirmation(self, method_frame):
         """Invoked by pika when RabbitMQ responds to a Basic.Publish RPC
@@ -184,51 +188,69 @@ class RabbitmqPublisher(object):
             '%i were acked and %i were nacked', self._message_number,
             len(self._deliveries), self._acked, self._nacked)
 
-    def example_publish_message_code(self):
-        """If the class is not stopping, publish a message to RabbitMQ,
-        appending a list of deliveries with the message number that was sent.
-        This list will be used to check for delivery confirmations in the
+    def _send_messages(self, channel):
+        """Publish a message to RabbitMQ, appending a set of deliveries with
+        the message number that was sent. This set will be used to
+        check for delivery confirmations in the
         _on_delivery_confirmations method.
-
-        Once the message has been sent, schedule another message to be sent.
-        The main reason I put scheduling in was just so you can get a good idea
-        of how the process is flowing by slowing down and speeding up the
-        delivery intervals by changing the PUBLISH_INTERVAL constant in the
-        class.
-
         """
-        if self._channel is None or not self._channel.is_open:
-            return
+        state = self._state
+        exchange = state.exchange
+        routing_key = state.routing_key
+        messages = state.messages
+        old_message_number = state.message_number
+        new_message_number = old_message_number + len(messages)
+        state.deliveries = set(range(old_message_number + 1, new_message_number + 1))
+        state.message_number = new_message_number
+        for m in messages:
+            channel.basic_publish(exchange, routing_key, m.body, m.properties)
+        LOGGER.debug('Published %i messages', len(messages))
 
-        hdrs = {u'مفتاح': u' قيمة', u'键': u'值', u'キー': u'値'}
-        properties = pika.BasicProperties(app_id='example-publisher',
-                                          content_type='application/json',
-                                          headers=hdrs)
+        # hdrs = {u'مفتاح': u' قيمة', u'键': u'值', u'キー': u'値'}
+        # properties = pika.BasicProperties(app_id='example-publisher',
+        #                                   content_type='application/json',
+        #                                   headers=hdrs)
 
-        message = u'مفتاح قيمة 键 值 キー 値'
-        self._channel.basic_publish(self.EXCHANGE, self.ROUTING_KEY,
-                                    json.dumps(message, ensure_ascii=False),
-                                    properties)
-        self._message_number += 1
-        self._deliveries[self._message_number] = True
-        LOGGER.info('Published message # %i', self._message_number)
-        self.schedule_next_message()
+        # message = u'مفتاح قيمة 键 值 キー 値'
+        # self._channel.basic_publish(self.EXCHANGE, self.ROUTING_KEY,
+        #                             json.dumps(message, ensure_ascii=False),
+        #                             properties)
+        # self._message_number += 1
+        # self._deliveries[self._message_number] = True
+        # LOGGER.info('Published message # %i', self._message_number)
+        # self.schedule_next_message()
 
-    def publish_messages(self, messages):
+    def publish_messages(self, messages, exchange, routing_key, *, allow_retry=True):
         """Publishes messages to RabbitMQ with delivery confirmation. This
         method blocks until a confirmation from the broker has been
         received for each of the messages.
         """
-        self._deliveries = {}
-        self._message_number = 0
-        connection = self._connection
+        state = self._state
+        state.messages = list(messages)
+        state.exchange = exchange
+        state.routing_key = routing_key
+
+        channel = self._channel
+        if channel is not None and channel.is_open:
+            self._send_messages(channel)
+
+        connection = self._get_connection()
         try:
             connection.ioloop.start()
         except KeyboardInterrupt:
-            connection.close()
+            if not (connection.is_closed or connection.is_closing):
+                connection.close()
             if not connection.is_closed:
-                # Loop until we're fully closed, will stop on its own.
+                # Start the loop again until the connection is fully
+                # closed. The loop should stop on its own.
                 connection.ioloop.start()
+            raise
+
+        # If at the end of the ioloop the connection is closed, most
+        # probably the connection has timed out. In this case, we
+        # should retry with a fresh connection, but only once.
+        if connection.is_closed and allow_retry:
+            return self.publish_messages(messages, exchange, routing_key, allow_retry=False)
 
 
 def main():
