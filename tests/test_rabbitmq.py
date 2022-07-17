fb84a874f7b685c905558db3654f3eea8d576715
Evgeni Pandurski <epandurski@gmail.com>
2022-07-17 17:39:42 +0300

    Add test

---
+++ tests/test_rabbitmq.py
@@ -0,0 +1,19 @@
+import pytest
+import pika
+from flask_signalbus.rabbitmq.publisher import RabbitmqPublisher, Message
+
+
+@pytest.fixture(params=['direct', 'init_app'])
+def publisher(app, request):
+    if request.param == 'direct':
+        p = RabbitmqPublisher(app)
+    elif request.param == 'init_app':
+        p = RabbitmqPublisher()
+        p.init_app(app)
+    return p
+
+
+@pytest.mark.skip('requires RabbitMQ instance running')
+def test_publisher(publisher):
+    message = Message('test message', pika.BasicProperties())
+    publisher.publish_messages([message], '', 'test')
